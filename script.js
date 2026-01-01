e083a8243293c424c51bca30c35fae9db1092e4a
Samsheikh0077 <samadabdultechozon@gmail.com>
2026-01-01 22:43:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-    const ads = [
-  { type: "image", src: "ads/ad1.jpg", time: 10, skip: true },
-  { type: "video", src: "ads/ad2.mp4", time: 10, skip: true },
-  { type: "image", src: "ads/ad3.jpg", time: 15, skip: false }
+const ads = [
+  { src: "ads/ad1.mp4", time: 10, skip: true },
+  { src: "ads/ad2.mp4", time: 10, skip: true },
+  { src: "ads/ad3.mp4", time: 15, skip: false }
 ];
 
 let current = 0;
-let timerInterval;
+let interval;
 
 const adBox = document.getElementById("adBox");
 const timer = document.getElementById("timer");
 const btn = document.getElementById("actionBtn");
 
 function loadAd() {
-  clearInterval(timerInterval);
+  clearInterval(interval);
   adBox.innerHTML = "";
 
   const ad = ads[current];
-  let element;
 
-  if (ad.type === "image") {
-    element = document.createElement("img");
-    element.src = ad.src;
-  } else {
-    element = document.createElement("video");
-    element.src = ad.src;
-    element.autoplay = true;
-    element.muted = true;
-  }
+  const video = document.createElement("video");
+  video.src = ad.src;
+  video.autoplay = true;
+  video.muted = true;      // autoplay ke liye zaroori
+  video.playsInline = true;
+  video.controls = false;
 
-  adBox.appendChild(element);
+  adBox.appendChild(video);
 
   let timeLeft = ad.time;
   btn.disabled = true;
   btn.innerText = ad.skip ? "Skip" : "Finish";
 
-  timerInterval = setInterval(() => {
+  interval = setInterval(() => {
     timer.innerText = `Wait ${timeLeft}s`;
     timeLeft--;
 
     if (timeLeft < 0) {
-      clearInterval(timerInterval);
+      clearInterval(interval);
       btn.disabled = false;
     }
   }, 1000);
@@ -50,7 +46,7 @@ btn.onclick = () => {
   if (current < ads.length) {
     loadAd();
   } else {
-    window.location.href = "https://dribbble.com/shots/25878898-SafeFund-S-F-Lettermark-Crypto-Logo-Design";
+    window.location.href = "https://your-final-link.com";
   }
 };
 
